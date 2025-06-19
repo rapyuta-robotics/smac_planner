184ca1e204b44a69a903116c3889b9e7caf917db
Nisarg236 <nisargnileshpanchal@gmail.com>
2025-06-19 16:41:05 +0900

    change by mistake

--- src/smac_planner_hybrid.cpp
+++ src/smac_planner_hybrid.cpp
@@ -217,7 +217,7 @@ uint32_t SmacPlannerHybrid::makePlan(
   align_pose_front.pose = Utils::getPoseAtDistanceAlongHeading(goal.pose, _search_info.goal_align_distance);
   align_pose_back.pose  = Utils::getPoseAtDistanceAlongHeading(goal.pose,  -_search_info.goal_align_distance);
 
-  if (!_config.allow_goal_overshoot) {
+  if (!_search_info.allow_goal_overshoot) {
     _search_info.setSearchBound(goal.pose);
     _search_info.setStart(start.pose.position);
     _a_star->setSearchBounds(goal.pose, start.pose.position, _search_info.allow_goal_overshoot);
