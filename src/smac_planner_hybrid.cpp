47b5b84a10f2b571cc1b2a1b38158c6b1dc075cb
Nisarg236 <nisargnileshpanchal@gmail.com>
2025-09-02 15:08:05 +0900

    change

--- src/smac_planner_hybrid.cpp
+++ src/smac_planner_hybrid.cpp
@@ -555,6 +555,7 @@ void SmacPlannerHybrid::getPath(
       plan_result.result_code = mbf_msgs::GetPathResult::BLOCKED_START;
       return;
     }
+    plan_result.result_code = result;
 
     if (result == mbf_msgs::GetPathResult::CANCELED) {
       plan_result.message = "Planner was cancelled";
@@ -567,10 +568,7 @@ void SmacPlannerHybrid::getPath(
     } else {
       plan_result.message = "No valid path found";
       plan_result.result_code = mbf_msgs::GetPathResult::NO_PATH_FOUND;
-      return;
     }
-    plan_result.result_code = result;
-    return;
   }
 
   // Convert to world coordinates
