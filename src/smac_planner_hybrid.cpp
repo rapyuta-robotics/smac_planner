249f531756a51550d67f27ea80890d7714c7f7ae
Nisarg236 <nisargnileshpanchal@gmail.com>
2025-09-02 14:35:17 +0900

    fix

--- src/smac_planner_hybrid.cpp
+++ src/smac_planner_hybrid.cpp
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License. Reserved.
 
+#include <iostream>
 #include <string>
 #include <memory>
 #include <vector>
@@ -558,14 +559,18 @@ void SmacPlannerHybrid::getPath(
 
     if (result == mbf_msgs::GetPathResult::CANCELED) {
       plan_result.message = "Planner was cancelled";
+      plan_result.result_code = mbf_msgs::GetPathResult::CANCELED;
     }
     else if (result == mbf_msgs::GetPathResult::PAT_EXCEEDED) {
       plan_result.message = "Exceeded maximum planning time";
+      plan_result.result_code = mbf_msgs::GetPathResult::PAT_EXCEEDED;
     }
     else if (num_iterations >= _a_star->getMaxIterations()) {
       plan_result.message = "Exceeded maximum iterations";
+      plan_result.result_code = mbf_msgs::GetPathResult::PAT_EXCEEDED;
     } else {
       plan_result.message = "No valid path found";
+      plan_result.result_code = mbf_msgs::GetPathResult::NO_PATH_FOUND;
     }
     return;
   }
